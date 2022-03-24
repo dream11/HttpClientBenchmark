338c131c910b0160fe23077d9e8a072764a2a7b7
Ubuntu <ubuntu@ip-172-31-21-10.ec2.internal>
2022-03-24 07:17:08 +0000

    cleaned run_server.sh

--- extra/run_server.sh
+++ extra/run_server.sh
@@ -1,5 +1,4 @@
 rm -rf /log/*.log
-#rm -rf /zio-http/example/target
 echo "$(pwd)" >> /log/server_out.log
 echo "BUILDING ASSEMBLY IN SERVER" >> /log/server_out.log
 cd /
@@ -9,9 +8,6 @@ echo "DONE BUILDING ASSEMBLY" >> /log/server_out.log
 
 # while sleep 2; do echo thinking; done
 
-# java --version >> /log/out.log
-# java -classpath /target/scala-2.12/client-benchmark-assembly-*.jar example.SimpleServer >> /log/out.log 
 java -classpath /target/scala-2.12/client-benchmark-assembly-0.1.0-SNAPSHOT.jar bench.SimpleServer >> /log/out.log 
 
-#java  -classpath /zio-http/jar/*assembly*.jar example.SimpleServer >> /log/server_out.log 
 
