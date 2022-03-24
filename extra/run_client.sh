c4b9f4f87bc7a211c9fa247fbaa5b6e51647d7e3
Ubuntu <ubuntu@ip-172-31-21-10.ec2.internal>
2022-03-24 07:15:41 +0000

    modified run_client.sh

--- extra/run_client.sh
+++ extra/run_client.sh
@@ -1,18 +1,4 @@
 rm -rf /log/*client*.log
-
 echo "$(pwd)" >> /log/client_out.log
-
-#rm -rf /zio-http/example/target
-# echo "BUILDING ASSEMBLY IN CLIENT" >> /log/client_out.log
-# cd /client/zio-http
-# echo ${pwd} >> /log/server_out.log
-# sbt "project example" clean assembly
-# sbt "project example" 
-# echo "DONE BUILDING ASSEMBLY IN CLIENT" >> /log/client_out.log
-
-# while sleep 2; do echo thinking; done
-
-# java --version >> /log/out.log
 java -classpath /target/scala-2.12/client-benchmark-assembly-0.1.0-SNAPSHOT.jar bench.ClientBenchmark >> /log/client_out.log
-#java  -classpath /zio-http/jar/*assembly*.jar example.SimpleServer >> /log/server_out.log 
 
