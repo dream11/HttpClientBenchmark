0d84ab6ebf1022c7a4dbf0d3a81bde83d2581124
Sumant Awasthi <sumant.awasthi@D11-Sumant.Awasthi>
2022-03-25 19:28:25 +0530

    use depends on for triggering client after server bootup

--- extra/run_server.sh
+++ extra/run_server.sh
@@ -6,8 +6,6 @@ cd /
 sbt assembly >> /log/assembly.log
 echo "DONE BUILDING ASSEMBLY" >> /log/server_out.log
 
-# while sleep 2; do echo thinking; done
-
 java -classpath /target/scala-2.12/client-benchmark-assembly-0.1.0-SNAPSHOT.jar bench.SimpleServer >> /log/out.log 
 
 
